f7e1b90fc5dcadd10901d224461124342a2e277b
michal <m.losak@safistudio.pl>
2020-02-27 13:24:34 +0100

    first commit

--- app/Http/Controllers/Auth/ActivateController.php
+++ app/Http/Controllers/Auth/ActivateController.php
@@ -5,8 +5,10 @@ namespace App\Http\Controllers\Auth;
 use App\Helpers\VerifyEmail;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\ActivateRequest;
+use App\Mail\User\SuccessRegisterMail;
 use App\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Mail;
 
 class ActivateController extends Controller
 {
@@ -25,6 +27,8 @@ class ActivateController extends Controller
                     'activated' => 1
                 ]);
             VerifyEmail::removeKey();
+
+            Mail::to(Auth::user()->email)->send(new SuccessRegisterMail());
 
             return redirect()->route('home')->with('front.activate.success');
         }
